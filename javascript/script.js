2a94214fc6b59648cc65fc20e1520efbf5153f92
wesshstos <weslleyhstos@gmail.com>
2022-05-21 03:47:07 -0300

    Primeiro Commit

---
+++ javascript/script.js
@@ -0,0 +1,70 @@
+let btnConsultar = document.querySelector('#btnConsultar')
+let cardResponse = document.querySelector('#cardResponse')
+let btnLimpar = document.querySelector('#btnLimpar')
+let dinheiro = document.querySelector('#dinheiro')
+
+let despesasFixas = document.querySelector('#despesasFixas')
+let despesasVariaveis = document.querySelector('#despesasVariaveis')
+let reservaFinanceira = document.querySelector('#reservaFinanceira')
+let labelInput = document.querySelector('#labelInput')
+
+btnLimpar.addEventListener('click', (e) =>{
+     e.preventDefault()
+
+     let dinheiro = document.querySelector('#dinheiro')
+     let cardRight = document.querySelector('.card-right')
+     let main = document.querySelector('main')
+
+     labelInput.removeAttribute('style', 'color: #f00')
+     labelInput.innerHTML = 'Valor Bruto Mensal'
+     dinheiro.removeAttribute('style', 'border: 2px solid #f00')
+
+     dinheiro.value = ''
+     document.documentElement.scrollTop = 0
+     main.style.justifyContent = 'center'
+     cardRight.style.display = 'none'
+})
+
+dinheiro.addEventListener('keyup', function(e){
+     e.preventDefault()
+
+     if (dinheiro.value.length === 0 || dinheiro.value.length <= 2) {
+          dinheiro.setAttribute('style', 'border: 2px solid #f00')
+          labelInput.innerHTML = 'Valor Bruto Mensal *Insira um valor de 100 até 9999'
+          labelInput.setAttribute('style', 'color: #f00')  
+     } else {
+          labelInput.removeAttribute('style', 'color: #f00')
+          labelInput.innerHTML = 'Valor Bruto Mensal'
+          dinheiro.removeAttribute('style', 'border: 2px solid #f00')
+     }
+})
+
+btnConsultar.addEventListener('click', function (e) {
+     e.preventDefault()
+
+     let dinheiro = document.querySelector('#dinheiro').value
+
+     if (dinheiro === '' || dinheiro.length <= 2) {
+          labelInput.innerHTML = 'Valor Bruto Mensal *Preencha este campo'
+          labelInput.setAttribute('style', 'color: #f00')
+     } else {
+
+          let valorSalario = parseFloat(dinheiro).toFixed(2)
+          let cardRight = document.querySelector('.card-right')
+          let main = document.querySelector('main')
+
+          let valorFixo = valorSalario * 0.5
+          let valorVariavel = valorSalario * 0.3
+          let valorReserva = valorSalario * 0.2
+
+          window.scrollTo(0, 789)
+          labelInput.removeAttribute('style', 'color: #f00')
+          labelInput.innerHTML = 'Valor Bruto Mensal'
+
+          main.style.justifyContent = 'space-around'
+          cardRight.style.display = 'flex'
+          despesasFixas.innerHTML = `R$ ${valorFixo.toFixed(2).replace('.', ',')}`
+          despesasVariaveis.innerHTML = `R$ ${valorVariavel.toFixed(2).replace('.', ',')}`
+          reservaFinanceira.innerHTML = `R$ ${valorReserva.toFixed(2).replace('.', ',')}`
+     }
+})
---
+++ readme.md
@@ -0,0 +1,51 @@
+<h1><strong>Projeto de calculo da regra 50-30-20</strong></h1>
+
+<p>
+     Este projeto fazer o calculo da Regra Financeira 50-30-20, onde o usuário saberá qual seu rendimento mensal devido o seu salario no mês.
+</p>
+<br>
+
+<h2 align="center">
+     ⚠ ReadMe | 🚀 Em constante Construção 🚧
+</h2>
+
+<h2>Sobre a Regra</h2>
+<hr>
+<br>
+<p align="justify">
+     O método 50-30-20 consiste em dividir a sua renda líquida mensal em três partes (50%, 30% e 20%), considerando as despesas fixas, despesas variáveis e o dinheiro que você irá poupar. O principal objetivo é que você consiga equilibrar as suas entradas e saídas e poupar dinheiro, entendendo qual será o destino do seu dinheiro.
+</p>
+<br>
+<a style="color: #eeeeee; background-color: #363645; padding: 10px; border-radius: 20px;" href="https://economia.uol.com.br/mais/pagbank/2021/10/14/regra-50-30-20-aprenda-um-metodo-para-conseguir-economizar-hoje.htm">Detalhes sobre a regra</a>
+<br>
+
+<br>
+<h1>Modo de Uso</h1>
+
+<ul>
+     <li>Preencha o campo de valor Bruto Mensal, use o valor do seu rendimento mensal para fazer o calculo.</li>
+     <img style="width: 500px; margin-bottom: 20px;" src="./github/exmpleone.gif" title="Readme" alt="Readme">
+     <li>Clique no botão consultar e ira abrir um card do lado direito</li>
+     <img style="margin-bottom: 20px;" src="./github/exempletwo.gif" title="Readme" alt="Readme">
+     <li>Caso queira fazer uma nova consulta, clique em Limpar</li>
+     <img style="margin-bottom: 20px;" src="./github/exemplethree.gif" title="Readme" alt="Readme">
+</ul>
+
+<h2>Evite esses Erros</h2>
+
+<ul>
+     <li>
+          Não deixe de preencher o campo
+     </li>
+     <img style="margin-bottom: 20px;" src="./github/exemplefour.gif" alt="Readme" title="Readme">
+     <li>
+          Coloque valores a partir do terceiro digito, o limite do campo é 8. Evite usar valores com centavos use somente numeros inteiros para fazer a consultas
+     </li>
+     <img style="margin-bottom: 20px;" src="./github/exemplefive.gif" alt="Readme" title="Readme">
+
+</ul>
+
+<h1>Tecnologias</h1>
+<p>
+     HTML5 • CSS3 • JAVASCRIPT
+</p>
